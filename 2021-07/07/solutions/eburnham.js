89a1de555e8802fe86e256b4f49119de591ab5dc
Emily Burnham <60051506+Aderyn1121@users.noreply.github.com>
2021-07-07 15:39:00 -0400

    Update eburnham.js

--- 2021-07/07/solutions/eburnham.js
+++ 2021-07/07/solutions/eburnham.js
@@ -1,36 +1,3 @@
-/* 
-
-Given an integer x, return true if x is palindrome integer.
-
-An integer is a palindrome when it reads the same 
-backward as forward. 
-For example, 121 is palindrome while 123 is not.
-
-Input: x = 121
-Output: true
-
-Input: x = -121
-Output: false
-Explanation: From left to right, it reads -121. 
-From right to left, it becomes 121-. 
-Therefore it is not a palindrome.
-
-Follow up: Could you solve it without 
-converting the integer to a string?
-
-*/
-
-
-
-
-//take in number
-//negative -> return false
-//check both ends of stringified number
-//call again til length = 2
-
-
-
-
 const palindrome = n => {
     if(Number(n) < 0) return false
     n = String(n)
