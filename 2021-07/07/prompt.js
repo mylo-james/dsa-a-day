ca7c21973bc5759daed55e6a501716556143bc16
Mylo James <mjames@appacademy.io>
2021-07-07 13:58:47 -0500

    2021-07-07

---
+++ 2021-07/07/prompt.js
@@ -0,0 +1,21 @@
+/* 
+
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same 
+backward as forward. 
+For example, 121 is palindrome while 123 is not.
+
+Input: x = 121
+Output: true
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. 
+From right to left, it becomes 121-. 
+Therefore it is not a palindrome.
+
+Follow up: Could you solve it without 
+converting the integer to a string?
+
+*/
