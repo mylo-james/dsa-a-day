d627816a802265e7bde61e552c17db2d3086476e
Ryan <rdmaloney3@gmail.com>
2021-07-08 13:44:35 -0400

    rmaloney string solution

--- 2021-07/08/solutions/rmaloney.js
+++ 2021-07/08/solutions/rmaloney.js
@@ -1,27 +1,54 @@
+//some improvement in time 76% better
 const lengthOfLongestSubstring = function (s) {
-  let max = 0
-  let sub = [];
-  let i = 0
+  let max = 0;
+  let sub = "";
+  let i = 0;
 
-  while(s[i]){
-    if(sub.includes(s[i])){
-      if(max < sub.length){
-        max = sub.length
-        debugger
-      }
-      sub = [s[i]]
-    }else{
-      sub.push(s[i])
+  while (s[i]) {
+    let dupIdx = sub.indexOf(s[i]);
+    if (dupIdx !== -1) {
+      sub = sub.slice(dupIdx + 1);
+    }
+    sub += s[i];
+
+    if (max < sub.length) {
+      max = sub.length;
     }
 
-    i += 1
+    i += 1;
   }
-  return max
 
+  return max;
 };
 
+//super slow implementation
+// const lengthOfLongestSubstring = function (s) {
+//   let max = 0;
+//   let sub = [];
+//   let i = 0;
 
-console.log(lengthOfLongestSubstring('abcabcbb'));
-console.log(lengthOfLongestSubstring('bbbbb'));
-console.log(lengthOfLongestSubstring('pwwkew'));
-console.log(lengthOfLongestSubstring(''));
+//   while (s[i]) {
+//     let dupIdx = sub.indexOf(s[i])
+//     if (dupIdx !== -1) {
+//       if (max < sub.length) {
+//         max = sub.length;
+//       }
+//       sub.push(s[i]);
+//       sub = sub.slice(dupIdx + 1);
+//     } else {
+//       sub.push(s[i]);
+//     }
+//     i += 1;
+//   }
+//   if (max < sub.length) {
+//     max = sub.length;
+//   }
+
+//   return max;
+// };
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
+console.log(lengthOfLongestSubstring("dvdf"));
+console.log(lengthOfLongestSubstring(" "));
